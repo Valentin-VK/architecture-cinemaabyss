3d0a6686db3b5b997fdb0b467d18388ed8176d49
Valentin-VK <kosevskih@gmail.com>
2025-11-03 01:52:05 +0300

    Sprint 2 - Add and fix helm

--- src/microservices/events/src/main/java/com/cinemaabyss/events/service/service/KafkaConsumerService.java
+++ src/microservices/events/src/main/java/com/cinemaabyss/events/service/service/KafkaConsumerService.java
@@ -1,6 +1,8 @@
 package com.cinemaabyss.events.service.service;
 
-import com.cinemaabyss.events.service.model.EventMessage;
+import com.cinemaabyss.events.service.model.MovieEventRequest;
+import com.cinemaabyss.events.service.model.PaymentEventRequest;
+import com.cinemaabyss.events.service.model.UserEventRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -11,9 +13,21 @@ public class KafkaConsumerService {
 
     private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
 
-    @KafkaListener(topics = "events-topic")
-    public void receive(String message) {
-        EventMessage event = EventMessage.fromJson(message); // Предположим метод парсинга JSON
+    @KafkaListener(topics = "${kafka.topic.movie-events}")
+    public void receiveMovieEvents(String message) {
+        MovieEventRequest event = MovieEventRequest.fromJson(message); // Предположим метод парсинга JSON
+        logger.info("Received event {}", event);
+    }
+
+    @KafkaListener(topics = "${kafka.topic.user-events}")
+    public void receiveUserEvents(String message) {
+        UserEventRequest event = UserEventRequest.fromJson(message); // Предположим метод парсинга JSON
+        logger.info("Received event {}", event);
+    }
+
+    @KafkaListener(topics = "${kafka.topic.payment-events}")
+    public void receivePaymentEvents(String message) {
+        PaymentEventRequest event = PaymentEventRequest.fromJson(message); // Предположим метод парсинга JSON
         logger.info("Received event {}", event);
     }
 }
--- src/microservices/events/src/main/java/com/cinemaabyss/events/service/service/KafkaProducerService.java
+++ src/microservices/events/src/main/java/com/cinemaabyss/events/service/service/KafkaProducerService.java
@@ -1,9 +1,11 @@
 package com.cinemaabyss.events.service.service;
 
 import com.cinemaabyss.events.service.model.EventMessage;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -19,8 +21,11 @@ public class KafkaProducerService {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendEvent(EventMessage event) {
-        kafkaTemplate.send("events-topic", event.toString());
-        logger.info("Sent event {}", event);
+    public void send(String topicName, int partition, String key, String value) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topicName, partition, key, value);
+        kafkaTemplate.send(record).addCallback(
+                result -> logger.info("Sent message to {} with key {}", topicName, key),
+                ex -> logger.error("Error sending message to {}: {}", topicName, ex)
+        );
     }
 }
